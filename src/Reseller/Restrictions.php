dc7abe5332b05ba96dc3c7a42d841a0285cc5465
John Saunders <john@yettimedia.co.uk>
2017-03-06 15:38:42 +0000

    Adding trait to restrictions

--- src/Reseller/Restrictions.php
+++ src/Reseller/Restrictions.php
@@ -1,6 +1,8 @@
 <?php
 namespace CashbackApi\Reseller;
 
+use CashbackApi\Restrictions\RestrictionTrait;
+
 /**
  * Class Restrictions
  * @package CashbackApi\Reseller
@@ -8,15 +10,8 @@ namespace CashbackApi\Reseller;
 class Restrictions extends BaseReseller
 {
 
-    /**
-     * @param bool $withDescription
-     * @return bool|object
-     */
-    public function getOfferTypes($withDescription = false)
-    {
-        $data = new \stdClass();
-        $data->with_description = $withDescription;
-        return $this->doRequest('reseller/restrictions/get-types', $data);
-    }
+    use RestrictionTrait;
+
+
 
 }
