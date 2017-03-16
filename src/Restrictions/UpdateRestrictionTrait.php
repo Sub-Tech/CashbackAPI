cc01f44399da72af79f15708931441d114bd3104
John Saunders <john@yettimedia.co.uk>
2017-03-16 14:05:53 +0000

    New restriction update methods

--- src/Restrictions/UpdateRestrictionTrait.php
+++ src/Restrictions/UpdateRestrictionTrait.php
@@ -10,13 +10,133 @@ trait UpdateRestrictionTrait
 {
     public $updateRestrictionPath;
 
-    public function getRestriction($restrictionId)
+    /**
+     * @param $restrictionId
+     * @param $resourceType
+     * @param $resourceId
+     * @return mixed
+     */
+    public function getRestriction($restrictionId, $resourceType, $resourceId)
     {
-        //TODO add get restriction
+        $data = new \stdClass();
+        $data->restriction_id = $restrictionId;
+        $data->restriction_resource_id = $resourceId;
+        $data->restriction_resource_type = $resourceType;
+
+        return $this->doRequest($this->updateRestrictionPath . '/restrictions/get', $data);
+
     }
 
-    public function updateRestriction($restrictionId, $restriction)
+    /**
+     * @param $restrictionId
+     * @param $resourceType
+     * @param $resourceId
+     * @param $restrictionData
+     * @return mixed
+     */
+    public function updateRestriction($restrictionId, $resourceType, $resourceId, $restriction = null)
     {
-        //TODO add update restriction
+        $data = $restriction ?? new \stdClass();
+        $data->restriction_id = $restrictionId;
+        $data->restriction_resource_id = $resourceId;
+        $data->restriction_resource_type = $resourceType;
+
+        return $this->doRequest($this->updateRestrictionPath . '/restrictions/update', $data);
     }
+
+    public function updateMinAge($restrictionId, $resourceType, $resourceId, $age)
+    {
+        $restriction = new \stdClass();
+        $restriction->restriction_type = 'minimum_age';
+        $restriction->minimum = $age;
+        return $this->updateRestriction($restrictionId, $resourceType, $resourceId, $restriction);
+    }
+
+    public function updateMaxAge($restrictionId, $resourceType, $resourceId, $age)
+    {
+        $restriction = new \stdClass();
+        $restriction->restriction_type = 'maximum_age';
+        $restriction->maximum_age = $age;
+        return $this->updateRestriction($restrictionId, $resourceType, $resourceId, $restriction);
+    }
+
+    public function updateIPRestriction($restrictionId, $resourceType, $resourceId, $ips)
+    {
+        $restriction = new \stdClass();
+        $restriction->restriction_type = 'ip_blacklist';
+        if (is_array($ips)) {
+            $restriction->ip_addresses = $ips;
+        } else {
+            $restriction->ip_address = $ips;
+        }
+        return $this->updateRestriction($restrictionId, $resourceType, $resourceId, $restriction);
+    }
+
+
+    /**
+     * @param $resourceType
+     * @param $resourceId
+     * @return \stdClass
+     */
+    protected function getUpdateResourceBlacklist($resourceType, $resourceId)
+    {
+        $restriction = new \stdClass();
+        $restriction->restriction_type = 'resource_blacklist';
+        $restriction->resource_id = $resourceId;
+        $restriction->resource_type = $resourceType;
+        return $restriction;
+    }
+
+    /**
+     * @param $restrictionId
+     * @param $resourceType
+     * @param $resourceId
+     * @param $categoryId
+     * @return mixed
+     */
+    public function updateCategoryRestriction($restrictionId, $resourceType, $resourceId, $categoryId)
+    {
+        $restriction = $this->getUpdateResourceBlacklist('category', $categoryId);
+        return $this->updateRestriction($restrictionId, $resourceType, $resourceId, $restriction);
+    }
+
+    /**
+     * @param $restrictionId
+     * @param $resourceType
+     * @param $resourceId
+     * @param $offerId
+     * @return mixed
+     */
+    public function updateOfferRestriction($restrictionId, $resourceType, $resourceId, $offerId)
+    {
+        $restriction = $this->getUpdateResourceBlacklist('offer', $offerId);
+        return $this->updateRestriction($restrictionId, $resourceType, $resourceId, $restriction);
+    }
+
+    /**
+     * @param $restrictionId
+     * @param $resourceType
+     * @param $resourceId
+     * @param $retailerId
+     * @return mixed
+     */
+    public function updateRetailerRestriction($restrictionId, $resourceType, $resourceId, $retailerId)
+    {
+        $restriction = $this->getUpdateResourceBlacklist('offer', $retailerId);
+        return $this->updateRestriction($restrictionId, $resourceType, $resourceId, $restriction);
+    }
+
+    /**
+     * @param $restrictionId
+     * @param $resourceType
+     * @param $resourceId
+     * @param $whitelabelId
+     * @return mixed
+     */
+    public function updateWhitelabelRestriction($restrictionId, $resourceType, $resourceId, $whitelabelId)
+    {
+        $restriction = $this->getUpdateResourceBlacklist('whitelabel', $whitelabelId);
+        return $this->updateRestriction($restrictionId, $resourceType, $resourceId, $restriction);
+    }
+
 }
